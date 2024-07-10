5660c7b0528d9b698ecfc563384eda0ee75794dc
Madison Hall <madisonhall@Madisons-Laptop.home>
2024-07-10 13:47:00 -0400

    Adding working combine data and data adjustments

--- combine_files.r
+++ combine_files.r
@@ -9,23 +9,20 @@ READ <- function(FILE) {
            trim_ws = TRUE,
            guess_max = nrow(read_csv(FILE)) -12,
            n_max = nrow(read_csv(FILE)) -12,
-           col_types = cols(
-             `Inception \nDate` = col_date("%m/%d/%Y"),
-             `Listing \nDate`= col_date("%m/%d/%Y")
-           )) %>% 
-    mutate(., filename = FILE) %>% 
+           # col_types = cols(
+           #   `Inception \nDate` = col_date("%m/%d/%Y"),
+           #   `Listing \nDate`= col_date("%m/%d/%Y")
+           # )
+        ) %>%
+    mutate(filename = basename(FILE)) %>%
     rename_all(make.names)
 }
 
 data <- files %>%
   map(READ) %>%    # read in all the files individually, using
   # the function READ from above
-  reduce(full_join)        # reduce with full_join into one dataframe
+  reduce(full_join, by=)        # reduce with full_join into one dataframe
 
 warnings <- warnings(data)
 
 write_csv(data, "data_combined.csv")
-
-a <- read_csv("data/MD_MF_20240408_5.csv")
-b <- read_csv("data/MD_MF_20240408_10.csv")
-c <- read_csv("data/MD_MF_20240408_15.csv")
